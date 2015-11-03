1b16ea8b2a6306affa4fd7298fd15ad19d2e248b
Wolfgang-at-SEAL <wolfgang.lang@sealsystems.de>
2015-11-03 10:52:52 +0100

    added to GIT

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var app = connect();
+var port = 3000;
+
+//server static files
+app.use(serveStatic(__dirname + "/public"));
+
+app.use(function (req, res, next) {
+  console.log(req.method + ': ' + req.url);
+  next();
+});
+
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+server.listen(port, function() {
+  console.log("Server listening on port " + port);
+});
+
+
+io.on('connection', function (socket) {
+  console.log("new connection");
+  socket.on('question', function (data) {
+    console.log("received question event ");
+    console.log(data);
+  });
+});
