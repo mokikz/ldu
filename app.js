48179e3fd7e6180e09ebb639b1dc0532f0ce3df0
WolfgangLang <kristina.und.wolfgang@t-online.de>
2017-07-24 01:02:01 +0200

    redirect index.html calls to project homepage

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var app = connect();
 var port = 3000;
+var url = require('url');
+
+// redirect index.html to jimdo homepage
+app.use(function (req, res, next){
+  if ((url.parse(req.url).pathname == '/')||
+     (url.parse(req.url).pathname == '/index.html')) {
+    res.writeHead(301, {Location: 'https://mokikz.jimdo.com/'});
+    res.end();
+  }
+  else {
+    next();
+  }
+});
+
+// send filtered data.js according to url options
+app.use(function (req, res, next){
+    next();
+});
 
 //server static files
 app.use(serveStatic(__dirname + "/public"));
