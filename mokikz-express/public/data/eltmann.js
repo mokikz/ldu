6b0d3d31bdbf5b6e45b99a3fadc8fbaceee476b1
WolfgangLang <kristina.und.wolfgang@t-online.de>
2022-06-29 22:49:26 +0200

    linter warnings

--- mokikz-express/public/data/eltmann.js
+++ mokikz-express/public/data/eltmann.js
@@ -1,34 +1,39 @@
-    var levels = [
-        {"world": "Start",
-         "klasse": [1,2,3],
-         "type": "Zeiger einstellen",
-         "levels": [
-           {"label": "Stell die Uhr auf ein Uhr", startHour: 10, startMinute: 0, zielHour: "01", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 3:00Uhr", startHour: 12, zielHour: 3, zielMinute: "00"},
-           {"label": "Stell die Uhr auf 10:00 Uhr", startHour: 6, startMinute: 0, zielHour: "10", zielMinute: "00"},
-           {"label": "Stell die Uhr auf zwei Uhr", startHour: 12, startMinute: 0, zielHour: "02", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 4:00Uhr", startMinute: 00, zielHour: 4, zielMinute: "00"},
-           {"label": "Stell die Uhr auf 7:00 Uhr", startHour: 6, startMinute: 0, zielHour: "07", zielMinute: "00"},
-           {"label": "Stell die Uhr auf fünf Uhr", startHour: 11, startMinute: 0, zielHour: "05", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 9:00 Uhr", startHour: 4, startMinute: 0, zielHour: "09", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 20:00 Uhr", startHour: 7, startMinute: 0, zielHour: "08", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 6:00 Uhr", startHour: 5, startMinute: 0, zielHour: "06", zielMinute: "00"},
-           {"label": "Stell die Uhr auf elf Uhr", startHour: 12, startMinute: 0, zielHour: "11", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 12 Uhr", startHour: 10, startMinute: 0, zielHour: "12", zielMinute: "00"},
-           {"label": "Stell die Uhr auf dreizehn Uhr", startHour: 10, startMinute: 0, zielHour: "01", zielMinute: "00"},
-           {"label": "Stell die Uhr auf dreiundzwanzig Uhr", startHour: 12, startMinute: "00", zielHour: "11", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 14 Uhr", startHour: 12, startMinute: 0, zielHour: "02", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 15:00Uhr", startHour: 12, zielHour: 3, zielMinute: "00"},
-           {"label": "Stell die Uhr auf 21:00 Uhr", startHour: 4, startMinute: 0, zielHour: "09", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 16:00Uhr", startMinute: 00, zielHour: 4, zielMinute: "00"},
-           {"label": "Stell die Uhr auf achtzehn Uhr", startHour: 5, startMinute: 0, zielHour: "06", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 8:00 Uhr", startHour: 3, startMinute: 0, zielHour: "08", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 17:00 Uhr", startHour: 11, startMinute: 0, zielHour: "05", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 22:00 Uhr", startHour: 6, startMinute: 0, zielHour: "10", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 19:00 Uhr", startHour: 3, startMinute: 0, zielHour: "07", zielMinute: "00"},
-           {"label": "Stell die Uhr auf 0 Uhr", startHour: 12, startMinute: 0, zielHour: "12", zielMinute: "00"}
-           ] },
-         {"world": "Intervalle",
+'use strict';
+const levels = [
+  {
+    world: 'Start',
+    klasse: [1, 2, 3],
+    type: 'Zeiger einstellen',
+    levels: [
+      {
+        "label": "Stell die Uhr auf ein Uhr", startHour: 10, startMinute: 0, zielHour: "01", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 3:00Uhr", startHour: 12, zielHour: 3, zielMinute: "00"},
+  {"label": "Stell die Uhr auf 10:00 Uhr", startHour: 6, startMinute: 0, zielHour: "10", zielMinute: "00"},
+  {"label": "Stell die Uhr auf zwei Uhr", startHour: 12, startMinute: 0, zielHour: "02", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 4:00Uhr", startMinute: 0, zielHour: 4, zielMinute: "00"},
+  {"label": "Stell die Uhr auf 7:00 Uhr", startHour: 6, startMinute: 0, zielHour: "07", zielMinute: "00"},
+  {"label": "Stell die Uhr auf fünf Uhr", startHour: 11, startMinute: 0, zielHour: "05", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 9:00 Uhr", startHour: 4, startMinute: 0, zielHour: "09", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 20:00 Uhr", startHour: 7, startMinute: 0, zielHour: "08", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 6:00 Uhr", startHour: 5, startMinute: 0, zielHour: "06", zielMinute: "00"},
+  {"label": "Stell die Uhr auf elf Uhr", startHour: 12, startMinute: 0, zielHour: "11", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 12 Uhr", startHour: 10, startMinute: 0, zielHour: "12", zielMinute: "00"},
+  {"label": "Stell die Uhr auf dreizehn Uhr", startHour: 10, startMinute: 0, zielHour: "01", zielMinute: "00"},
+  {"label": "Stell die Uhr auf dreiundzwanzig Uhr", startHour: 12, startMinute: "00", zielHour: "11", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 14 Uhr", startHour: 12, startMinute: 0, zielHour: "02", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 15:00Uhr", startHour: 12, zielHour: 3, zielMinute: "00"},
+  {"label": "Stell die Uhr auf 21:00 Uhr", startHour: 4, startMinute: 0, zielHour: "09", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 16:00Uhr", startMinute: 0, zielHour: 4, zielMinute: "00"},
+  {"label": "Stell die Uhr auf achtzehn Uhr", startHour: 5, startMinute: 0, zielHour: "06", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 8:00 Uhr", startHour: 3, startMinute: 0, zielHour: "08", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 17:00 Uhr", startHour: 11, startMinute: 0, zielHour: "05", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 22:00 Uhr", startHour: 6, startMinute: 0, zielHour: "10", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 19:00 Uhr", startHour: 3, startMinute: 0, zielHour: "07", zielMinute: "00"},
+  {"label": "Stell die Uhr auf 0 Uhr", startHour: 12, startMinute: 0, zielHour: "12", zielMinute: "00"}
+    ]
+  },
+  {
+    "world": "Intervalle",
          "klasse": [1,2,3],
          "type": "Zeiger einstellen",
          "levels": [
@@ -43,11 +48,11 @@
           "klasse": [1,2,3],
           "type": "Zeit eingeben",
           "levels": [
-            {"label": "Wie viel Zeit ist seit 18:00 Uhr vergangen?", startHour: 19, startMinute: 00, zielHour: "1", zielMinute: "0"},
+            {"label": "Wie viel Zeit ist seit 18:00 Uhr vergangen?", startHour: 19, startMinute: 0, zielHour: "1", zielMinute: "0"},
             {"label": "Wie lange ist es noch bis 13:00 Uhr?", startHour: 12, startMinute: 0, zielHour: "1", zielMinute: "0"},
             {"label": "Wie viel Zeit ist seit 15:00 Uhr vergangen?", startHour: 18, startMinute: 0, zielHour: "3", zielMinute: "0"},
             {"label": "Wie lange ist es noch bis 12:00 Uhr?", startHour: 8, startMinute: 0, zielHour: "4", zielMinute: "0"},
-            {"label": "Der Bus fährt um 13:00 Uhr. Wie lange dauert es noch?", startHour: 11, startMinute: 00, zielHour: "1", zielMinute: "0"},
+            {"label": "Der Bus fährt um 13:00 Uhr. Wie lange dauert es noch?", startHour: 11, startMinute: 0, zielHour: "1", zielMinute: "0"},
             {"label": "Wann ist das Kino aus, wenn der Film um 16:00 Uhr angefangen hat und zwei Stunden dauert?", startHour: 16, startMinute: 0, zielHour: "18", zielMinute: "0"},
             ] },
         {"world": "Halbe und viertel Stunden",
@@ -148,11 +153,11 @@
          "type": "Zeiger einstellen",
          "levels": [
            {"label": "Max muss zu seinem Schulbus 10 min laufen. Wann muss er zu Hause los, damit er den Bus um 7:23 Uhr erwischt?", startHour: 0, startMinute: 0, zielHour: "07", zielMinute: "13"},
-           {"label": "Lena geht um 15:00 Uhr zum Einkaufen. Sie braucht 14 min für den Bäcker und 17min im Supermarkt. Wann kommt sie wieder zurück?", startHour: 15, startMinute: 00, zielHour: "15", zielMinute: "31"},
+           {"label": "Lena geht um 15:00 Uhr zum Einkaufen. Sie braucht 14 min für den Bäcker und 17min im Supermarkt. Wann kommt sie wieder zurück?", startHour: 15, startMinute: 0, zielHour: "15", zielMinute: "31"},
            {"label": "Felix und Nils treffen sich um 15:45 Uhr im Schwimmbad. Sie dürfen 120 Minuten bleiben. Wann müssen sie wieder gehen?", startHour: 15, startMinute: 45, zielHour: "17", zielMinute: "45"},
-           {"label": "Nadja will mit ihrer Freundin um 14:15 Uhr ins Kino. Die Karten müssen sie eine halbe Stunde vorher an der Kasse abholen.Wann müssen sie im Kino sein?", startHour: 00, startMinute: 00, zielHour: "13", zielMinute: "45"},
+           {"label": "Nadja will mit ihrer Freundin um 14:15 Uhr ins Kino. Die Karten müssen sie eine halbe Stunde vorher an der Kasse abholen.Wann müssen sie im Kino sein?", startHour: 0, startMinute: 0, zielHour: "13", zielMinute: "45"},
            {"label": "Der Bus ins Kino fährt ab zwölf Uhr alle 20 Minuten und braucht 27 Minuten für die Fahrt. Welchem Bus muss Nadja nehmen, damit sie um 13:45 Uhr an der Kinokasse ist?", startHour: 13, startMinute: 45, zielHour: "13", zielMinute: "00"},
-           {"label": "Nach dem Kino um 16:15 Uhr möchte Nadja noch eine halbe Stunde ein Eis essen. Wann fährt der nächste Bus, wenn er immer noch alle 20 min fährt?", startHour: 16, startMinute: 15, zielHour: "17", zielMinute: "00"},
+           {"label": "Nach dem Kino um 16:15 Uhr möchte Nadja noch eine halbe Stunde ein Eis essen. Wann fährt der nächste Bus, wenn er immer noch alle 20 min fährt?", startHour: 16, startMinute: 15, zielHour: "17", zielMinute: "00"}
            ] },
         {"world": "Zeitspannen 1 - Wie viel Zeit ist vergangen?",
          "klasse": [2,3],
@@ -163,20 +168,20 @@
            {"label": "Wie viel Zeit ist zwischen 12:25 Uhr und 12:35 Uhr vergangen?", startHour: 12, startMinute: 0, zielHour: "12", zielMinute: "10"},
            {"label": "Wie viel Zeit ist zwischen 7:15 Uhr und 9:30 Uhr vergangen?", startHour: 12, startMinute: 0, zielHour: "2", zielMinute: "15"},
            {"label": "Wie viel Zeit ist zwischen 11:45 Uhr und 13:10 Uhr vergangen?", startHour: 12, startMinute: 0, zielHour: "1", zielMinute: "25"},
-           {"label": "Wie viel Zeit ist zwischen 18:11 Uhr und 19:27 Uhr vergangen?", startHour: 12, startMinute: 0, zielHour: "1", zielMinute: "16"},
+           {"label": "Wie viel Zeit ist zwischen 18:11 Uhr und 19:27 Uhr vergangen?", startHour: 12, startMinute: 0, zielHour: "1", zielMinute: "16"}
            ] },
          {"world": "Zeitspannen 2 - Wann endet die Veranstaltung?",
          "klasse": [3],
          "type": "Zeiger einstellen",
          "levels": [
            {"label": "Wann endet die 90 minütige Sportstunde, die um 11:20 Uhr beginnt?", startHour: 11, startMinute: 20, zielHour: "12", zielMinute: "50"},
-           {"label": "Wann endet das Konzert, das um 20:09 Uhr beginnt und 1h 7min dauert??", startHour: 20, startMinute: 09, zielHour: "21", zielMinute: "16"},
+           {"label": "Wann endet das Konzert, das um 20:09 Uhr beginnt und 1h 7min dauert??", startHour: 20, startMinute: 9, zielHour: "21", zielMinute: "16"},
            {"label": "Wann endet die Fahrt in die Ferien, die um 13:27 Uhr beginnt und 2 h 50 min dauert?", startHour: 13, startMinute: 0, zielHour: "16", zielMinute: "17"},
-           {"label": "Wann endet der Wandertag, der um 8:10 Uhr beginnt und 3 h 50 min dauert?", startHour: 8, startMinute: 00, zielHour: "12", zielMinute: "00"},
+           {"label": "Wann endet der Wandertag, der um 8:10 Uhr beginnt und 3 h 50 min dauert?", startHour: 8, startMinute: 0, zielHour: "12", zielMinute: "00"},
            {"label": "Wann endet die Pause, die um 9:30 Uhr beginnt und 25 min dauert?", startHour: 9, startMinute: 30, zielHour: "9", zielMinute: "55"},
            {"label": "Wann geht Lena ins Bett, wenn sie um 17:45 Uhr noch 1 h 45 min aufbleiben darf?", startHour: 17, startMinute: 45, zielHour: "19", zielMinute: "30"},
            {"label": "Wann fährt Kilian in die Schule, wenn er um 6:30 Uhr aufsteht und nach 55 min das Haus verlässt?", startHour: 6, startMinute: 30, zielHour: "7", zielMinute: "25"},
-           {"label": "Wann kommt Antonia nach Hause, wenn sie mit ihren Eltern am Sonntag um 11:30 Uhr 2 h 15 min lang zum Mittagessen geht?", startHour: 11, startMinute: 15, zielHour: "13", zielMinute: "45"},
+           {"label": "Wann kommt Antonia nach Hause, wenn sie mit ihren Eltern am Sonntag um 11:30 Uhr 2 h 15 min lang zum Mittagessen geht?", startHour: 11, startMinute: 15, zielHour: "13", zielMinute: "45"}
            ] },
          {"world": "Zeitspannen 3",
          "klasse": [3],
@@ -189,6 +194,6 @@
            {"label": "In wie viel Minuten fährt der Bus 14:42 Uhr?", startHour: 13, startMinute: 47, zielHour: "0", zielMinute: "55"},
            {"label": "In wie viel Minuten fährt der Bus 13:42 Uhr?", startHour: 13, startMinute: 17, zielHour: "0", zielMinute: "25"},
            {"label": "Wie viel Minuten darfst du noch im Schwimmbad bleiben, wenn du um 15:00 Uhr gekommen bist und für 90 Minuten bezahlt hast?", startHour: 15, startMinute: 33, zielHour: "0", zielMinute: "57"},
-           {"label": "In wie viel Minuten ist das Kino aus, wenn der Film um 16:00 Uhr angefangen hat und 120 Minuten dauert?", startHour: 17, startMinute: 33, zielHour: "0", zielMinute: "27"},
+           {"label": "In wie viel Minuten ist das Kino aus, wenn der Film um 16:00 Uhr angefangen hat und 120 Minuten dauert?", startHour: 17, startMinute: 33, zielHour: "0", zielMinute: "27"}
            ] }
   ];
