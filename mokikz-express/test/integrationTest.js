cab376f1632df02517d11b1f7fcdd019a20e4827
WolfgangLang <kristina.und.wolfgang@t-online.de>
2022-07-17 17:36:09 +0200

    work in progress

--- mokikz-express/test/integrationTest.js
+++ mokikz-express/test/integrationTest.js
@@ -8,13 +8,13 @@ test('Integration test', async function () {
   this.timeout = 10 * 60 * 1000;
   console.log(`Hallo`);
   await new Promise((resolve) => {
-      supertest(app)
-        .get('/data.js')
-        .send()
-        .set('Accept', 'application/json')
-        .expect(200)
-        .end ((err, res) => {
-            assert.that(err).is.null();
-        })
+    supertest(app)
+      .get('/data.js')
+      .send()
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        assert.that(err).is.null();
+      });
   });
 });
